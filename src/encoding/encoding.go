2c4cd31896520d47e352fa6ed0095226de78bf6f
PatriTyk <188729543+KaporMirr@users.noreply.github.com>
2024-12-06 06:02:34 +0100

    encoding.go

--- src/encoding/encoding.go
+++ src/encoding/encoding.go
@@ -1,47 +1,7 @@
-package encoding
 
-import (
-	"strings"
-	"unicode/utf8"
-
-	"github.com/saintfish/chardet"
-)
-
-func DetectCharset(content []byte) (string, error) {
-	detector := chardet.NewTextDetector()
-	result, err := detector.DetectBest(content)
-	if err != nil {
-		return "", err
 	}
 
-	return strings.ToUpper(result.Charset), nil
-}
-
-func ToUTF8(content string) []byte {
-	b := make([]byte, len(content))
-	i := 0
-	for _, r := range content {
-		i += utf8.EncodeRune(b[i:], r)
-	}
-
-	return b[:i]
-}
-
-// func ToUTF8(content []byte, ignoreInvalidITF8Chars ...bool) (string, error) {
-// 	ignore := false
-// 	if len(ignoreInvalidITF8Chars) > 0 {
-// 		ignore = ignoreInvalidITF8Chars[0]
-// 	}
-
-// 	cs, err := DetectCharset(content)
-// 	if err != nil {
-// 		if !ignore {
-// 			return "", err
-// 		}
-// 		cs = "UTF-8"
-// 	}
-
-// 	bs := string(content)
+	
 // 	if ignore {
 // 		if !utf8.ValidString(bs) {
 // 			v := make([]rune, 0, len(bs))
